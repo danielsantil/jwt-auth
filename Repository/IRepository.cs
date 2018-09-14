983a5072dee3fbe143adc6330ec8289dde35b7a1
danielsantil <daniel.shcf@gmail.com>
2018-09-14 17:02:59 -0400

    added repository

---
+++ Repository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace JwtAuth.Repository
+{
+    public interface IRepository<T>
+    {
+        //Task<T> Get(int id);
+        Task<T> Get(Expression<Func<T, bool>> expression);
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> expression);
+        Task Insert(T entity);
+        Task Update(T entity);
+        Task UpdateAttach(T entity, Action<T> changes);
+        //Task Delete(T entity);
+        //Task Delete(int id);
+        void Detach(T entity);
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,71 @@
+﻿using JwtAuth.DataContext;
+using JwtAuthModels.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace JwtAuth.Repository
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        private readonly JwtAuthDbContext _context;
+
+        public Repository(JwtAuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<T> Get(Expression<Func<T, bool>> expression)
+        {
+            T entity = await _context.Set<T>().FirstOrDefaultAsync(expression);
+            Detach(entity);
+            return entity;
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return await _context.Set<T>().Where(expression).ToListAsync();
+        }
+
+        public virtual async Task Insert(T entity) {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task Update(T entity) {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        
+        public virtual async Task UpdateAttach(T entity, Action<T> changes)
+        {
+            _context.Set<T>().Attach(entity);
+            changes.Invoke(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task Delete(T entity) {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task Delete(int id) {
+            T entity = await Get(x => x.Id == id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Detach(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Detached;
+        }
+    }
+}
